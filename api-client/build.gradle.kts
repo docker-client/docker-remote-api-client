00a55bdc999dfb6f96f66bd001ffe670fd6af157
Tobias Gesellchen <tobias@gesellix.de>
2021-10-09 20:37:56 +0200

    fix dependency conflict

--- api-client/build.gradle.kts
+++ api-client/build.gradle.kts
@@ -23,9 +23,9 @@ dependencies {
   implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.5.31")
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2")
   implementation("com.squareup.moshi:moshi:1.12.0")
-//  implementation("com.squareup.moshi:moshi-kotlin:1.12.0")
   kapt("com.squareup.moshi:moshi-kotlin-codegen:1.12.0")
-  implementation("com.squareup.okhttp3:okhttp:4.9.2")
+  implementation("com.squareup.okhttp3:okhttp:[4.9,5)!!4.9.2")
+//  implementation("com.squareup.okhttp3:logging-interceptor:[4.9,5)!!4.9.2")
   testImplementation("io.kotlintest:kotlintest-runner-junit5:3.4.2")
   implementation("de.gesellix:docker-remote-api-model-${remoteApiVersion.replace(".", "-")}:2021-10-04T21-50-00")
   implementation("de.gesellix:docker-engine:2021-09-29T15-30-00")
@@ -33,11 +33,6 @@ dependencies {
 
   implementation("org.slf4j:slf4j-api:[1.7,)!!1.7.32")
   testImplementation("ch.qos.logback:logback-classic:[1.2,2)!!1.2.6")
-
-//  implementation("com.squareup.okhttp3:logging-interceptor:4.9.1")
-//  implementation("org.apache.commons:commons-lang3:3.10")
-//  implementation("javax.annotation:javax.annotation-api:1.3.2")
-//  testImplementation("junit:junit:4.13.1")
 }
 
 tasks.withType(Test::class.java) {
