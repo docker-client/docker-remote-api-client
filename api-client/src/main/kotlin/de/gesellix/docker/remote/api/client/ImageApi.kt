9cd97d10ea103e42a356e725257866bfbc826c82
Tobias Gesellchen <tobias@gesellix.de>
2021-11-13 11:08:45 +0100

    Allow custom callbacks at imageBuild()

--- api-client/src/main/kotlin/de/gesellix/docker/remote/api/client/ImageApi.kt
+++ api-client/src/main/kotlin/de/gesellix/docker/remote/api/client/ImageApi.kt
@@ -15,12 +15,14 @@ import de.gesellix.docker.engine.DockerClientConfig
 import de.gesellix.docker.engine.RequestMethod.DELETE
 import de.gesellix.docker.engine.RequestMethod.GET
 import de.gesellix.docker.engine.RequestMethod.POST
+import de.gesellix.docker.remote.api.BuildInfo
 import de.gesellix.docker.remote.api.BuildPruneResponse
 import de.gesellix.docker.remote.api.ContainerConfig
 import de.gesellix.docker.remote.api.HistoryResponseItem
 import de.gesellix.docker.remote.api.IdResponse
 import de.gesellix.docker.remote.api.Image
 import de.gesellix.docker.remote.api.ImageDeleteResponseItem
+import de.gesellix.docker.remote.api.ImageID
 import de.gesellix.docker.remote.api.ImagePruneResponse
 import de.gesellix.docker.remote.api.ImageSearchResponseItem
 import de.gesellix.docker.remote.api.ImageSummary
@@ -169,6 +171,7 @@ class ImageApi(dockerClientConfig: DockerClientConfig = defaultClientConfig, pro
   // with Dockerfiles relying on `buildx`. Maybe we can simply shell out to a locally installed docker cli,
   // but that's something the user might solve themselves.
   @Throws(UnsupportedOperationException::class, ClientException::class, ServerException::class)
+  @JvmOverloads
   fun imageBuild(
     dockerfile: String?,
     t: String?,
@@ -196,7 +199,8 @@ class ImageApi(dockerClientConfig: DockerClientConfig = defaultClientConfig, pro
     platform: String?,
     target: String?,
     outputs: String?,
-    inputStream: InputStream
+    inputStream: InputStream,
+    callback: StreamCallback<BuildInfo?>? = null, timeoutMillis: Long? = null /*= 24.hours.toLongMilliseconds()*/
   ) {
     val localVariableConfig = imageBuildRequestConfig(
       dockerfile = dockerfile,
@@ -228,25 +232,25 @@ class ImageApi(dockerClientConfig: DockerClientConfig = defaultClientConfig, pro
       inputStream = inputStream
     )
 
-    val localVarResponse = requestStream<Any?>(
+    val localVarResponse = requestStream<BuildInfo?>(
       localVariableConfig
     )
 
-    val timeout = Duration.of(1, ChronoUnit.MINUTES)
-    // TODO collect events and/or extract the image id
-    // from either `aux ID=sha:.*`,
-    // or `stream Successfully built .*`,
-    // or `stream Successfully tagged .*` messages.
-    val callback = LoggingCallback<Any>()
+    val timeout = if (timeoutMillis == null) {
+      Duration.of(10, ChronoUnit.MINUTES)
+    } else {
+      Duration.of(timeoutMillis, ChronoUnit.MILLIS)
+    }
+    val actualCallback = callback ?: LoggingCallback<BuildInfo?>()
 
     return when (localVarResponse.responseType) {
       ResponseType.Success -> {
         runBlocking {
           launch {
             withTimeout(timeout.toMillis()) {
-              callback.onStarting(this@launch::cancel)
-              (localVarResponse as SuccessStream<*>).data.collect { callback.onNext(it) }
-              callback.onFinished()
+              actualCallback.onStarting(this@launch::cancel)
+              ((localVarResponse as SuccessStream<*>).data as Flow<BuildInfo?>).collect { actualCallback.onNext(it) }
+              actualCallback.onFinished()
             }
           }
         }
@@ -261,6 +265,30 @@ class ImageApi(dockerClientConfig: DockerClientConfig = defaultClientConfig, pro
       ResponseType.ServerError -> {
         val localVarError = localVarResponse as ServerError<*>
         throw ServerException("Server error : ${localVarError.statusCode} ${localVarError.message.orEmpty()}", localVarError.statusCode, localVarResponse)
+      }
+    }
+  }
+
+  fun getImageId(buildInfos: List<BuildInfo>): ImageID? {
+    val firstAux = buildInfos.stream()
+      .filter { (_, _, _, _, _, _, _, aux): BuildInfo -> aux != null }
+      .findFirst()
+    if (firstAux.isPresent) {
+      return firstAux.get().aux
+    } else {
+      val idFromStream = buildInfos.stream()
+        .filter { (_, stream): BuildInfo -> stream?.contains("Successfully built ")!! }
+        .findFirst()
+      return if (idFromStream.isPresent) {
+        ImageID(idFromStream.get().stream!!.removePrefix("Successfully built ").replaceAfter('\n', "").trim())
+      } else {
+        val tagFromStream = buildInfos.stream()
+          .filter { (_, stream): BuildInfo -> stream?.contains("Successfully tagged ")!! }
+          .findFirst()
+        tagFromStream.map { (_, stream): BuildInfo ->
+          ImageID(stream!!.removePrefix("Successfully tagged ").replaceAfter('\n', "").trim())
+        }
+          .orElse(null)
       }
     }
   }
---
+++ api-client/src/test/java/de/gesellix/docker/remote/api/client/ImageApiTest.java
@@ -0,0 +1,55 @@
+package de.gesellix.docker.remote.api.client;
+
+import de.gesellix.docker.engine.DockerClientConfig;
+import de.gesellix.docker.remote.api.BuildInfo;
+import de.gesellix.docker.remote.api.ImageID;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ImageApiTest {
+
+  private ImageApi imageApi;
+
+  @BeforeEach
+  public void setup() {
+    imageApi = new ImageApi(new DockerClientConfig());
+  }
+
+  @Test
+  public void getImageIdFromAux() {
+    List<BuildInfo> infos = new ArrayList<>();
+    infos.add(new BuildInfo(null, null, null, null, null, null, null, new ImageID("sha256:expected-id")));
+    infos.add(new BuildInfo(null, "Successfully built f9d5f290d048\nfoo bar", null, null, null, null, null, null));
+    infos.add(new BuildInfo(null, "Successfully tagged image:tag\nbar baz", null, null, null, null, null, null));
+
+    ImageID imageId = imageApi.getImageId(infos);
+
+    assertEquals("sha256:expected-id", imageId.getID());
+  }
+
+  @Test
+  public void getImageIdFromStreamWithBuildMessage() {
+    List<BuildInfo> infos = new ArrayList<>();
+    infos.add(new BuildInfo(null, "Successfully built f9d5f290d048\nfoo bar", null, null, null, null, null, null));
+    infos.add(new BuildInfo(null, "Successfully tagged image:tag\nbar baz", null, null, null, null, null, null));
+
+    ImageID imageId = imageApi.getImageId(infos);
+
+    assertEquals("f9d5f290d048", imageId.getID());
+  }
+
+  @Test
+  public void getImageIdFromStreamWithTagMessage() {
+    List<BuildInfo> infos = new ArrayList<>();
+    infos.add(new BuildInfo(null, "Successfully tagged image:tag\nbar baz", null, null, null, null, null, null));
+
+    ImageID imageId = imageApi.getImageId(infos);
+
+    assertEquals("image:tag", imageId.getID());
+  }
+}
