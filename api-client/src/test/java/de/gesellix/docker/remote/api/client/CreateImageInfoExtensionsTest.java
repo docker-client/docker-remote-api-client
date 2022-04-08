4cc62ea6aad088251797f1a7529708a39552dbd9
Tobias Gesellchen <tobias@gesellix.de>
2022-04-08 22:42:48 +0200

    chore

--- api-client/src/test/java/de/gesellix/docker/remote/api/client/CreateImageInfoExtensionsTest.java
+++ api-client/src/test/java/de/gesellix/docker/remote/api/client/CreateImageInfoExtensionsTest.java
@@ -13,9 +13,9 @@ class CreateImageInfoExtensionsTest {
   @Test
   public void getImageIdFromStatus() {
     List<CreateImageInfo> infos = new ArrayList<>();
-    infos.add(new CreateImageInfo("os-linux", null, "Pulling from gesellix/echo-server", null, null));
+    infos.add(new CreateImageInfo("example", null, "Pulling from gesellix/echo-server", null, null));
     infos.add(new CreateImageInfo(null, null, "Digest: sha256:04c0275878dc243b2f92193467cb33cdb9ee2262be64b627ed476de73e399244", null, null));
-    infos.add(new CreateImageInfo(null, null, "Status: Image is up to date for gesellix/echo-server:os-linux", null, null));
+    infos.add(new CreateImageInfo(null, null, "Status: Image is up to date for gesellix/echo-server:example", null, null));
     infos.add(new CreateImageInfo(null, null, "sha256:87f5e747ad067f91a7c4adf154deea20cebc3a749be5c2864a0c65cf70ddd8c4", null, null));
 
     String imageId = CreateImageInfoExtensionsKt.getImageId(infos);
