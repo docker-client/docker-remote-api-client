f5912360eb5199eaa5d41d9fabb0b0edf76ad671
Tobias Gesellchen <tobias@gesellix.de>
2022-10-01 21:56:00 +0200

    Bump logback-classic to 1.3.1

--- api-client/build.gradle.kts
+++ api-client/build.gradle.kts
@@ -73,7 +73,7 @@ dependencies {
   implementation("de.gesellix:docker-filesocket:2022-07-28T00-22-00")
 
   implementation("org.slf4j:slf4j-api:2.0.3")
-  testImplementation("ch.qos.logback:logback-classic:[1.2,2)!!1.2.11")
+  testImplementation("ch.qos.logback:logback-classic:[1.2,2)!!1.3.1")
 
   testImplementation("io.kotlintest:kotlintest-runner-junit5:3.4.2")
   testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.0")
