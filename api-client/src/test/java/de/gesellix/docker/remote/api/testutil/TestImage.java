1e145670dc6991eeed8ede65d1e2e6b1714b5798
Tobias Gesellchen <tobias@gesellix.de>
2023-12-19 10:57:07 +0100

    Bump echo-server

--- api-client/src/test/java/de/gesellix/docker/remote/api/testutil/TestImage.java
+++ api-client/src/test/java/de/gesellix/docker/remote/api/testutil/TestImage.java
@@ -16,7 +16,7 @@ public class TestImage {
 
     this.useWindowsContainer = Objects.requireNonNull(engineApiClient.getSystemApi().systemVersion().getOs()).equalsIgnoreCase("windows");
     this.repository = "gesellix/echo-server";
-    this.tag = "2023-07-02T12-00-00";
+    this.tag = "2023-12-19T09-50-00";
 
     // TODO consider NOT calling prepare inside the constructor
     prepare();
