21cc83f4962ab68a93c9bb5ce66d0daa88f940f6
Tobias Gesellchen <tobias@gesellix.de>
2024-01-16 21:44:35 +0100

    Bump tetsutil dependency

--- api-client/build.gradle.kts
+++ api-client/build.gradle.kts
@@ -84,7 +84,7 @@ dependencies {
   testImplementation("org.junit.platform:junit-platform-commons:1.10.1")
 
   testImplementation("org.apache.commons:commons-compress:1.25.0")
-  testImplementation("de.gesellix:testutil:[2023-08-30T01-01-01,)")
+  testImplementation("de.gesellix:testutil:[2024-01-01T01-01-01,)")
   testImplementation("de.gesellix:docker-registry:2024-01-16T20-10-00")
 }
 
