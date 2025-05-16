63c0704e836f6181d1be93654b076c1b0e2ce6e5
Tobias Gesellchen <tobias@gesellix.de>
2025-05-17 01:24:08 +0200

    Bump api version

--- api-client/src/test/java/de/gesellix/docker/remote/api/client/SystemApiIntegrationTest.java
+++ api-client/src/test/java/de/gesellix/docker/remote/api/client/SystemApiIntegrationTest.java
@@ -141,7 +141,7 @@ class SystemApiIntegrationTest {
   @Test
   public void systemVersion() {
     SystemVersion systemVersion = systemApi.systemVersion();
-    assertTrue(asList("1.42", "1.43", "1.44", "1.45", "1.46", "1.47").contains(systemVersion.getApiVersion()));
+    assertTrue(asList("1.42", "1.43", "1.44", "1.45", "1.46", "1.47", "1.48").contains(systemVersion.getApiVersion()));
   }
 
   static class SystemEventsCallback implements StreamCallback<EventMessage> {
